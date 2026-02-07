cbbd954e2e886ce8d2a0e9b4fcd2bb973fb68e50
Mona Sriji <mosriji@f1r8s20.clusters.42paris.fr>
2026-02-07 15:45:55 +0100

    7912

--- 42CommonCore/M.2/push_swap/final_supposed_product.c
+++ 42CommonCore/M.2/push_swap/final_supposed_product.c
@@ -6,51 +6,52 @@
 /*   By: mosriji <mosriji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 10:59:55 by mosriji           #+#    #+#             */
-/*   Updated: 2026/02/07 11:50:45 by mosriji          ###   ########.fr       */
+/*   Updated: 2026/02/07 15:45:25 by mosriji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int get_max_bits(int size)
+int	get_max_bits(int size)
 {
-    int bits;
-    
-    bits = 0;
-    while ((size - 1) >> bits)
-        bits++;
-    return (bits);
+	int	bits;
+
+	bits = 0;
+	while ((size - 1) >> bits)
+		bits++;
+	return (bits);
 }
 
-void sort_by_bit(t_list **stack_a, t_list **stack_b, int bit_index)
+void	sort_by_bit(t_list **stack_a, t_list **stack_b, int bit_index)
 {
-    int size;
-    int i;
-    
-    size = lst_size(*stack_a);
-    i = 0;
-    while (i < size)
-    {
-        if ((((*stack_a)->index >> bit_index) & 1) == 0)
-            push_b(stack_a, stack_b);
-        else
-            rotate_a(stack_a);
-        i++;
-    }
-    while (*stack_b)
-        push_a(stack_b, stack_a);
+	int	size;
+	int	i;
+
+	i = 0;
+	size = lst_size(*stack_a);
+	while (i < size)
+	{
+		if ((((*stack_a)->index >> bit_index) & 1) == 0)
+			push_b(stack_a, stack_b);
+		else
+			rotate_a(stack_a);
+		i++;
+	}
+	while (*stack_b)
+		push_a(stack_b, stack_a); // pousse tout ce qu'on a mit
+									// dans la stack b (qui contient les 1)
 }
 
-void radix_sort(t_list **stack_a, t_list **stack_b)
+void	radix_sort(t_list **stack_a, t_list **stack_b)
 {
-    int max_bits;
-    int i;
-    
-    max_bits = get_max_bits(lst_size(*stack_a));
-    i = 0;
-    while (i < max_bits)
-    {
-        sort_by_bit(stack_a, stack_b, i);
-        i++;
-    }
+	int	max_bits;
+	int	bit;
+
+	bit = 0;
+	max_bits = get_max_bits(lst_size(*stack_a));
+	while (bit < max_bits)
+	{
+		sort_by_bit(stack_a, stack_b, bit);
+		bit++;
+	}
 }
